1f83da0f9713c9d33d7e7a302923508685f3830f
miscbits <willow6876@gmail.com>
2015-05-21 12:52:09 -0400

    added second page to route

--- hello.py
+++ hello.py
@@ -1,9 +1,15 @@
+import os
+
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def home():
     return 'Hello World!'
 
+@app.route('/page/')
+def page(): 
+	return 'This is a second page'
+	
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0')

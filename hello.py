b31cb01b33ac411f8131feb31b114c6e8b703832
miscbits <willow6876@gmail.com>
2015-05-21 22:41:22 -0400

    added templates for html pages and made first route with parameters to pass through

--- hello.py
+++ hello.py
@@ -1,15 +1,17 @@
 import os
 
+from flask import render_template
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello World!'
+    return render_template('home.html')
 
-@app.route('/page/')
-def page(): 
-	return 'This is a second page'
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
 	
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
